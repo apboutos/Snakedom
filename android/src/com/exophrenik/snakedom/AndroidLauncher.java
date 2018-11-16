6f2552c5597406f2108ae82e04c4c5432372dcaf
Apostolis Boutos <exophrenik@gmail.com>
2018-11-16 16:03:03 +0200

    First commit

---
+++ android/src/com/exophrenik/snakedom/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.exophrenik.snakedom;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.exophrenik.snakedom.Snakedom;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Snakedom(), config);
+	}
+}
---
+++ desktop/src/com/exophrenik/snakedom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.exophrenik.snakedom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.exophrenik.snakedom.Snakedom;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snakedom(), config);
+	}
+}
